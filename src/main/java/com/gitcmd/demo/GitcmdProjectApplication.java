d0968a634036e890959a787374d07b90e4a158c8
Prasad Dhanikonda <prasadsource@gmail.com>
2020-09-22 19:54:31 +0530

    first commit

---
+++ src/main/java/com/gitcmd/demo/GitcmdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gitcmd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcmdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitcmdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitcmd/demo/GitcmdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitcmd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitcmdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
