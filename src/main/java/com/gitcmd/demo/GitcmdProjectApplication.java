6482d7147e04a23a5a3a793522423a782cf27cca
Prasad Dhanikonda <prasadsource@gmail.com>
2020-09-22 20:18:10 +0530

    This is branch1 commit

--- src/main/java/com/gitcmd/demo/GitcmdProjectApplication.java
+++ src/main/java/com/gitcmd/demo/GitcmdProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitcmdProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitcmdProjectApplication.class, args);
+	public static void main(String[] args1) {
+		SpringApplication.run(GitcmdProjectApplication.class, args1);
 	}
 
 }
